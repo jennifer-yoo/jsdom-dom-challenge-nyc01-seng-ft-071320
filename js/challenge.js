3f9d3cef73ec5411b094ea27be5826299e4aad73
Alex <alex.beciana@gmail.com>
2020-08-26 16:27:38 -0400

    Third deliverable completed

--- js/challenge.js
+++ js/challenge.js
@@ -11,12 +11,34 @@ document.addEventListener("DOMContentLoaded", function(){
     const plusButton = document.getElementById("plus")
 
     minusButton.addEventListener("click", function(e) {
-        //if (e.target.id === "minus") {
             counterInt -= 1
-      //  }
     })
 
     plusButton.addEventListener("click", function(e) {
         counterInt += 1
     })
+
+    const like = document.getElementById("heart")
+    const likeContainer = document.querySelector('.likes')
+    
+    like.addEventListener("click", function(e) {
+        
+        let numberLikes = document.querySelectorAll('.likeNumber')
+        let likeArr = Array.from(numberLikes)
+        
+        let x = likeArr.find(ele => ele.dataset.counter === counterInt.toString())
+        
+        if(x) {
+            console.dir(x)
+            
+            let currentLikes = parseInt(x.textContent.split(' ')[2])
+            
+            x.innerText = `${counterInt} - ${currentLikes += 1} likes`
+        } else {
+            likeContainer.innerHTML += `<li class="likeNumber" data-counter="${counterInt}">${counterInt} - 1 likes</li>`
+            
+        }
+        
+    })
+    
 })
